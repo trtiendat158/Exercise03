34defa8f07a45e65e254094691bf05bac183e76b
PC <PC@DESKTOP-1OI91SQ>
2019-09-26 13:14:31 +0700

    Problem Squence

---
+++ Exercise03/Squence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1();
+        }
+        static void Problem1()
+        {
+            //1.	Write a program to output the following (text) figure:
+
+            Console.WriteLine("_______________¶______");
+            Console.WriteLine("| Love OOP&C#  ||l\"\"|\"\"\\_,_ ");
+            Console.WriteLine("|______________|||__|__|__|] ");
+            Console.WriteLine(" (@)@)**********(@)(@)**(@)");
+        }
+    }
+}
